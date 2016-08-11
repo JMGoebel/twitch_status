6db69a1c988589b094085ad151fe5cb6fb25264a
Jason Goebel <goebel.jason@gmail.com>
2016-08-10 21:46:14 -0500

    created layout

--- README.md
+++ README.md
@@ -17,30 +17,21 @@ hackerrank:   https://www.hackerrank.com/goebel_jason
 
 FreeCodeCamp: https://www.freecodecamp.com/jmgoebel
 
-
-
-
-### Show the Local Weather
-
 #### Objective:  Build a CodePen.io app that is functionally similar to this: https://codepen.io/FreeCodeCamp/full/Myvqmo/
 
-1. [ ] User can see whether Free Code Camp is currently streaming on Twitch.tv.
+- [ ] User can see whether Free Code Camp is currently streaming on Twitch.tv.
 
-2. [ ] User can click the status output and be sent directly to the Free Code Camp's Twitch.tv channel.
+- [ ] User can click the status output and be sent directly to the Free Code Camp's Twitch.tv channel.
 
-3. [ ] If a Twitch user is currently streaming, I can see additional details about what they are streaming.
+- [ ] If a Twitch user is currently streaming, I can see additional details about what they are streaming.
 
-4. [ ] User will see a placeholder notification if a streamer has closed their Twitch account (or the account never existed). 
+- [ ] User will see a placeholder notification if a streamer has closed their Twitch account (or the account never existed). 
     You can verify this works by adding brunofin and comster404 to your array of Twitch streamers.
 
 
+Test Case
+["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas", "brunofin", "comster404"]
 
-
-#### --twitch.tv API--
-To pull local weather data
-
-#### --Illustrator--
-For svn icons
 
 #### --Special Thanks--
 freeCodeCamp.com for guidence
--- script.js
+++ script.js
@@ -1,9 +1,38 @@
 /*global $, console */
 
-$(document).ready(function () {
+function displayData(data) {
+  'use strict';
+  $("#display").text(data.follows);
+
+}
+
+function addStream(followerData) {
   'use strict';
 
-  var twitchAPI = 'https://api.twitch.tv/kraken/channels/test_channel',
+  var pullStreams = 'https://api.twitch.tv/kraken/streams/',
+    client_id = 'fnp03zxl6kktdbte1vp01h5wk4y65aj';
+
+  followerData.follows.forEach(function (user, index) {
+    var userName = user.channel.display_name;
+
+    $.getJSON(pullStreams + userName, {
+      client_id: client_id
+    })
+      .fail(function (data) {
+        console.error("Retrieval from twitch has failed.", data);
+      })
+      .done(function (data) {
+        followerData.follows[index].stream = data.stream;
+      });
+  });
+
+  displayData(followerData);
+}
+
+function gatherChannels(userName) {
+  'use strict';
+
+  var twitchAPI = 'https://api.twitch.tv/kraken/users/' + userName + '/follows/channels',
     client_id = 'fnp03zxl6kktdbte1vp01h5wk4y65aj';
 
   $.getJSON(twitchAPI, {
@@ -13,8 +42,24 @@ $(document).ready(function () {
       console.error("Retrieval from twitch has failed.", data);
     })
     .done(function (data) {
-      console.log(data);
+      addStream(data);
     });
+}
+
+$(document).ready(function () {
+  'use strict';
+
+  $('li:first-child()').addClass('active');
+
+  gatherChannels("jmgoebel03");
+
+  $('li').click(function () {
+    $('li').removeClass('active');
+    $(this).addClass('active');
+  });
+
+
 });
 
 // cid: fnp03zxl6kktdbte1vp01h5wk4y65aj
+
